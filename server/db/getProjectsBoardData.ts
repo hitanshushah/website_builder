0aea5f516d5b5eecc97508d5e5e68cb9c1097136
hitanshushah <hitanshushah10.8@gmail.com>
2025-10-27 01:18:01 -0400

    fix: /10

--- server/db/getProjectsBoardData.ts
+++ server/db/getProjectsBoardData.ts
@@ -424,7 +424,7 @@ export async function getPublicWebsiteData(userId: number): Promise<ProjectsBoar
                     'sorting_order', pr.sorting_order,
                     'created_at', pr.created_at,
                     'updated_at', pr.updated_at,
-                    'category', c.key,
+                    'category', c.name,
                     'status', s.key,
                     -- Project Settings
                     'settings', (
